947783c1704f9956fafb8bb1a1a141669fe9b47d
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2018-01-07 14:30:26 +0100

    *end docker

--- backend/docker/build.sh
+++ backend/docker/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+cd ..
 mvn clean install
-mv target/*.jar target/app.jar
-docker build -t trondheimdc/submit-backend docker
+mv target/*.jar docker/app.jar
+cd docker
+docker build -t trondheimdc/submit-backend .
 #docker push trondheimdc/submit-backend
+rm -rf app.jar
