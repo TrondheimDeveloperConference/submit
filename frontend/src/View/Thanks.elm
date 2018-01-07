ed21703d08cbf27856ac281a6d5f08e657c94260
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2018-01-07 18:17:49 +0100

    TDC styles

--- frontend/src/View/Thanks.elm
+++ frontend/src/View/Thanks.elm
@@ -9,7 +9,7 @@ view : Html Msg
 view =
     div [ class "wrapper login " ]
         [ div [ class "logo-wrapper" ]
-            [ img [ src "assets/neon-logo.svg", class "logo" ] []
+            [ img [ src "assets/tdc-logo.svg", class "logo" ] []
             ]
         , h1 [] [ text "Awesome!", br [] [], text " We are on it!" ]
         , div [ class "success-image-wrapper" ]
