beaf57c296edb22cfe3eac3bda5a0a12799870d2
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2019-01-06 17:31:05 +0100

    Building in Docker for travis

--- backend/docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ..
-mvn clean install
-mv target/*.jar docker/app.jar
-cd docker
-docker build -t trondheimdc/submit-backend .
-docker push trondheimdc/submit-backend
-rm -rf app.jar
